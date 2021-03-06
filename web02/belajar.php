18dd06b1da11fadccc5efdfc9c34ecdbdb675c82
shaliah28 <shaliahputri123@gmail.com>
2021-03-06 16:33:02 +0700

    php dasar

---
+++ web02/belajar.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nama = 'shaliah';
+    $angka = 12;
+    $angka_desimal = 12.7;
+
+    echo $nama;
+    echo "<br>";
+    echo $angka;
+    echo "<br>";
+
+    echo 'hello word! selamat hati jum"at';
+
+    echo "<h1>hello word</h1>";
+
+
+?> 
